ed50186082f8190b1bfb876ece963e20ac25d42a
aydev <aayush.kumar@famepilot.com>
2021-01-10 23:29:24 +0530

    Feature 9 - Lock Unlock

---
+++ Assets/Scripts/Level/LevelManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    private static LevelManager instance;
+    public static LevelManager Instance { get { return instance; } }
+
+    private void Awake() {
+        if (instance == null){
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start() {
+        if (GetLevelStatus((int)SceneEnum.Level1) == LevelStatus.Locked){
+            SetLevelStatus(SceneEnum.Level1, LevelStatus.Unlocked);
+        }
+    }
+
+    public LevelStatus GetLevelStatus(int levelIndex) {
+        return (LevelStatus)PlayerPrefs.GetInt(Utils.getLevelNameByIndex(levelIndex), (int)LevelStatus.Locked);
+        // Scene selectedScene = SceneManager.GetSceneByBuildIndex(levelIndex);
+    }
+
+    public void SetLevelStatus(SceneEnum level, LevelStatus levelStatus) {
+        string levelName = Utils.getLevelNameByIndex((int)level);
+        PlayerPrefs.SetInt(levelName, (int)levelStatus);
+        Debug.Log("Level: " + levelName + ", Status: " + levelStatus);
+
+    }
+}
---
+++ Assets/Scripts/Level/LevelOverController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelOverController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.GetComponent<PlayerController>() != null){
+            Debug.Log("Level is completed");
+            int currentLevelIndex = SceneManager.GetActiveScene().buildIndex;
+            int nextLevelIndex = currentLevelIndex + 1;
+            LevelManager.Instance.SetLevelStatus((SceneEnum)currentLevelIndex, LevelStatus.Completed);
+            LevelManager.Instance.SetLevelStatus((SceneEnum)nextLevelIndex, LevelStatus.Unlocked);
+            SceneManager.LoadScene(nextLevelIndex);
+        }
+    }
+}
